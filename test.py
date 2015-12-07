7defb62df7c6d949147e7145000642dbf7413a2d
ningdegang <ningdegang@foxmail.com>
2015-12-07 17:46:51 +0800

    path

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import sys,os
-sys.path.append(os.path.dirname(__file__))
+#sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from model.models import *
 
 if __name__ == "__main__":
+    for p in sys.path: print p
     test()
