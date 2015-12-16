85fe7d963f89f4a10fbe0e9affa56a407ef3c038
ningdegang <ningdegang@foxmail.com>
2015-12-16 18:11:31 +0800

    rm test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys,os
-#sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from model.models import *
-
-if __name__ == "__main__":
-    for p in sys.path: print p
-    test()
